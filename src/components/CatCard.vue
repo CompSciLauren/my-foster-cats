0a904a43fa30a1e7bda5e9e7590890f27a7fe538
Lauren Stephenson <CompSciLauren@gmail.com>
2021-06-29 17:33:08 -0500

    Add cat card component, edit page contents

---
+++ src/components/CatCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="CatCardContainer">
+    <h1>{{ name }}</h1>
+    <p>Sex: {{ sex }}</p>
+    <p>Birthday: {{ birthday }}</p>
+    <p>Breed: {{ breed }}</p>
+    <p>Color: {{ color }}</p>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CatWorld',
+  props: {
+    name: String,
+    sex: String,
+    birthday: String,
+    breed: String,
+    color: String,
+    description: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.CatCardContainer {
+    margin: 8px 8px;
+    border: 2px dotted green;
+    background-color: #c0f3e6;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,37 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="My Foster Cats"/>
+    <div class="CatCardsContainer">
+      <CatWorld class="CatCardItem" name="Noni" sex="Male" birthday="04/01/2021" breed="Tabby" color="White and Tabby" description="Text."/>
+      <CatWorld class="CatCardItem" name="Tucker" sex="Male" birthday="04/01/2021" breed="Tabby" color="White and Tabby" description="Text."/>
+      <CatWorld class="CatCardItem" name="Chan" sex="Male" birthday="04/01/2021" breed="Siamese" color="Cream" description="Text."/>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/Home.vue'
+import CatWorld from '@/components/CatCard.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    CatWorld
   }
 }
 </script>
+
+<style scoped>
+.CatCardsContainer {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  grid-gap: 10px;
+  padding: 10px;
+}
+
+.CatCardItem {
+  grid-row: 1 / span 2;
+}
+</style>
